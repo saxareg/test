41795888030dfcfdb8bc27c4ec92bb7c2cddf813
Ivan <saxareg2007@gmail.com>
2025-07-10 17:16:12 +0300

    Adding option 2

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 some_list = [1, 2, 3, 1, 'hello', 3, 'hello']
 
+# opition 1
+
 
 def make_original(lst):
     original_list = []
@@ -7,6 +9,14 @@ def make_original(lst):
         if item in original_list:
             continue
         original_list.append(item)
+    return original_list
+
+
+def other_make_original(lst):
+    original_list = []
+    for item in lst:
+        if item not in original_list:
+            original_list.append(item)
     return original_list
 
 
